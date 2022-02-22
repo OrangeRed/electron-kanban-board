2e18c156b1743346adec5a2de1124897af9d1e7f
Dmitriy <congo.dimtree@gmail.com>
2022-02-22 00:15:17 -0500

    Added project styling, need to fix margin performace bug

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,39 @@ import { v4 as uuidv4 } from "uuid"
 import Column from "./components/Column"
 import { KanbanItem, KanbanList } from "./types"
 
-const colItems: KanbanItem[] = [
-  { id: uuidv4(), title: "First", content: "first task" },
-  { id: uuidv4(), title: "Second", content: "second task" },
+import "./App.css"
+
+const testColItems: KanbanItem[] = [
+  {
+    id: uuidv4(),
+    title: "🔹 Solar Exchange ",
+    content: "Create Solar Exchange UI",
+  },
+  {
+    id: uuidv4(),
+    title: "🔹 Learn Formik",
+    content: "Simple Formik project ( . . . )",
+  },
+  {
+    id: uuidv4(),
+    title: "🔺 EP-1855",
+    content: "Add totals/averages to the bottom of energy dashboard table",
+  },
+  {
+    id: uuidv4(),
+    title: "🔺 EP-1855",
+    content: "Add totals/averages to the bottom of energy dashboard table",
+  },
 ]
 
-const cols: KanbanList = {
+const testCols: KanbanList = {
   [uuidv4()]: {
-    title: "To do",
-    items: colItems,
+    title: "To Do",
+    items: testColItems,
+  },
+  [uuidv4()]: {
+    title: "In Progress",
+    items: [],
   },
   [uuidv4()]: {
     title: "Done",
@@ -20,48 +44,70 @@ const cols: KanbanList = {
   },
 }
 
-const App = () => {
-  const [columns, setColumns] = useState(cols)
+/** TODO LIST
+ *  1) Improve performance on dragging cards (padding / margin issue)
+ *    1.1) https://github.com/atlassian/react-beautiful-dnd/issues/1855
+ *  2) Add button to make new column
+ *  3) Add button to make new task
+ *  4) Add button to edit task
+ *  5) Make columns draggable
+ */
 
-  const onDragEnd = (result: DropResult) => {
-    if (!result?.destination) return
-    const { source, destination } = result
-    const srcColumn = columns[source.droppableId]
-    const cpSrcItems = [...srcColumn.items]
-    const destColumn = columns[destination.droppableId]
-    const cpDestItems = [...destColumn.items]
-    const [removed] = cpSrcItems.splice(source.index, 1)
+const onDragEnd = (result: DropResult, columns: KanbanList): KanbanList => {
+  if (!result?.destination) return columns
+  const { source, destination } = result
+  const srcColumn = columns[source.droppableId]
+  const cpSrcItems = [...srcColumn.items]
+  const destColumn = columns[destination.droppableId]
+  const cpDestItems = [...destColumn.items]
+  const [removed] = cpSrcItems.splice(source.index, 1)
 
-    // Change item position
-    if (source.droppableId === destination.droppableId) {
-      cpSrcItems.splice(destination.index, 0, removed)
-      setColumns({
-        ...columns,
-        [source.droppableId]: {
-          ...srcColumn,
-          items: cpSrcItems,
-        },
-      })
-    } else {
-      cpDestItems.splice(destination.index, 0, removed)
-      setColumns({
-        ...columns,
-        [source.droppableId]: {
-          ...srcColumn,
-          items: cpSrcItems,
-        },
-        [destination.droppableId]: {
-          ...destColumn,
-          items: cpDestItems,
-        },
-      })
+  // Change item position
+  if (source.droppableId === destination.droppableId) {
+    cpSrcItems.splice(destination.index, 0, removed)
+    return {
+      ...columns,
+      [source.droppableId]: {
+        ...srcColumn,
+        items: cpSrcItems,
+      },
+    }
+  } else {
+    cpDestItems.splice(destination.index, 0, removed)
+    return {
+      ...columns,
+      [source.droppableId]: {
+        ...srcColumn,
+        items: cpSrcItems,
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: cpDestItems,
+      },
     }
   }
+}
+
+const App = () => {
+  const [columns, setColumns] = useState(testCols)
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
-      <DragDropContext onDragEnd={(result) => onDragEnd(result)}>
-        {Object.entries(columns).map(([id, colItems]) => {
+    <div
+      className="container"
+      style={{
+        height: "100%",
+        width: "100%",
+        overflowX: "auto",
+        margin: 0,
+        display: "flex",
+        justifyContent: "center",
+        overflowY: "hidden",
+      }}
+    >
+      <DragDropContext
+        onDragEnd={(result) => setColumns(onDragEnd(result, columns))}
+      >
+        {Object.entries(columns).map(([colId, colItems]) => {
           return (
             <div
               style={{
@@ -70,8 +116,7 @@ const App = () => {
                 alignItems: "center",
               }}
             >
-              <h2 style={{ color: "white" }}>{colItems.title}</h2>
-              <Column id={id} col={colItems} />
+              <Column colId={colId} colItems={colItems} />
             </div>
           )
         })}
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -2,28 +2,69 @@ import React from "react"
 import { Draggable, Droppable } from "react-beautiful-dnd"
 import { KanbanColumn } from "../types"
 
+import "./Column.css"
+
 type ColumnProps = {
-  id: string
-  col: KanbanColumn
+  colId: string
+  colItems: KanbanColumn
 }
 
-const Column = ({ id, col }: ColumnProps) => {
+const Column = ({ colId, colItems }: ColumnProps) => {
   return (
-    <div style={{ margin: 8 }}>
-      <Droppable droppableId={id}>
+    <div
+      className="column-container"
+      style={{
+        margin: 8,
+        padding: 5,
+        borderRadius: "5px",
+        backgroundColor: "#fefefe",
+      }}
+    >
+      <div
+        className="column-head"
+        style={{
+          width: "100%",
+          flexDirection: "column",
+          justifyContent: "left",
+          alignItems: "left",
+          paddingLeft: 15,
+        }}
+      >
+        <div style={{ margin: 0, fontSize: "1.4rem" }}>{colItems.title}</div>
+        <span style={{ fontSize: "14px", opacity: "40%" }}>{`${
+          colItems.items.length
+        } ${colItems.items.length === 1 ? "Card" : "Cards"}`}</span>
+      </div>
+      <Droppable droppableId={colId}>
         {(provided, snapshot) => {
           return (
             <div
+              className="list-container"
               ref={provided.innerRef}
               {...provided.droppableProps}
               style={{
-                background: snapshot.isDraggingOver ? "orange" : "darkorange",
-                padding: 4,
-                width: 250,
-                minHeight: 300,
+                position: "relative",
+                padding: 5,
+                background: "#dedede",
+                borderRadius: "5px",
+                overflowY: "scroll",
+                minHeight: 200,
+                minWidth: 282,
               }}
             >
-              {col.items.map((item, index) => {
+              <div
+                className="top-box-shadow"
+                style={{
+                  position: "sticky",
+                  top: -7,
+                  width: "100%",
+                  height: "0.6rem",
+                  marginBottom: 10,
+                  pointerEvents: "none",
+                  zIndex: 1,
+                }}
+              ></div>
+              {colItems.items.map((item, index) => {
                 return (
                   <Draggable key={item.id} draggableId={item.id} index={index}>
                     {(provided, snapshot) => {
@@ -32,29 +73,89 @@ const Column = ({ id, col }: ColumnProps) => {
                           ref={provided.innerRef}
                           {...provided.draggableProps}
                           {...provided.dragHandleProps}
+                          className={
+                            "card-container" +
+                            (snapshot.isDragging ? "is-dragging" : undefined)
+                          }
                           style={{
+                            position: "relative",
+                            width: 270,
+                            backgroundColor: "#ffffff",
+                            // margin: "8px 4px 8px 4px", <-- Performance issue / Non fluid drags
+                            borderRadius: "5px",
                             userSelect: "none",
-                            padding: 16,
-                            margin: "0 0 8px 0",
-                            minHeight: "50px",
-                            backgroundColor: snapshot.isDragging
-                              ? "lightblue"
-                              : "cyan",
                             ...provided.draggableProps.style,
                           }}
                         >
-                          <h3 style={{ marginTop: 0 }}>{item.title}</h3>
-                          <div>{item.content}</div>
+                          <div
+                            className="card"
+                            style={{
+                              padding: "4px 12px 4px 12px",
+                              fontSize: 14,
+                            }}
+                          >
+                            <h1
+                              style={{
+                                margin: 0,
+                                fontSize: "1.1rem",
+                                lineHeight: "1.3rem",
+                              }}
+                            >
+                              {item.title}
+                            </h1>
+                            <div
+                              style={{
+                                margin: "10px 0px 10px 0px",
+                                opacity: "60%",
+                                maxHeight: 250,
+                              }}
+                            >
+                              {item.content}
+                            </div>
+                            <hr
+                              style={{
+                                opacity: "10%",
+                                marginBottom: 25,
+                              }}
+                            ></hr>
+                          </div>
                         </div>
                       )
                     }}
                   </Draggable>
                 )
               })}
+              {provided.placeholder}
+              <div
+                className="bottom-box-shadow"
+                style={{
+                  content: "",
+                  marginTop: 20,
+                  position: "sticky",
+                  bottom: -7,
+                  width: "100%",
+                  height: "0.6rem",
+                  pointerEvents: "none",
+                  zIndex: 1,
+                }}
+              ></div>
             </div>
           )
         }}
       </Droppable>
+      <div
+        style={{
+          background: "white",
+          padding: "4px 0",
+          textAlign: "center",
+          position: "sticky",
+          bottom: 0,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <button>+</button>
+      </div>
     </div>
   )
 }
